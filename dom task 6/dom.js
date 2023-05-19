4b323fba4f86164cd1b566fee0b8fa8300435889
Vasudevsuthar <133574340+Vasudevsuthar@users.noreply.github.com>
2023-05-19 21:56:47 +0530

    Add files via upload

---
+++ dom task 6/dom.js
@@ -0,0 +1,9 @@
+// QUERYSELECTOR //
+var lastItem= document.querySelector(".list-group-item:last-child");
+      
+       var SecondItem=document.querySelector(".list-group-item:nth-child(2)");
+       var ThirdItem=document.querySelector(".list-group-item:nth-child(3)");
+
+       SecondItem.style.backgroundColor='green'
+       ThirdItem.textContent='';
+
